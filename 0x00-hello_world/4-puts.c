5cfe643bfc9fe994b1b54aced56cd5040f2dc4d1
kwamena98 <derrickdadson14@gmail.com>
2023-03-19 10:53:51 -0700

    DIamonds

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+/**
+ *main - A program that prints a lint with puts function
+ *Return: 0 (Success)
+ */
 
-int main (void){
+
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	return (0);
 }
